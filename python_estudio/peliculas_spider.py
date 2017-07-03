341d3448b79efded6d68ff586ca063c156a7fa17
carlos <carlos@cishao.cn>
2017-07-03 17:20:44 +0800

    test

--- python_estudio/peliculas_spider.py
+++ python_estudio/peliculas_spider.py
@@ -48,4 +48,4 @@ peliculas = Peliculas_Spider('http://www.ygdy8.net')
 list = peliculas.getPeliculaList(160)
 downloadlink = peliculas.getPeliculaUrl(list)
 peliculas.SaveList(downloadlink)
-
+#test

1c2d36e0a14fe9783fe2a1fc0515f1ef79223364
carlos <carlos@cishao.cn>
2018-01-10 18:04:09 +0800

    07-06修改,dytt_spider将抓取数据保存到mysql中

--- python_estudio/beautifulsoup/1222.py
+++ python_estudio/beautifulsoup/1222.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup as bs
 resq =  urllib.urlopen('https://es.wikipedia.org/wiki/Colombia').read()
 soup = bs(resq,'html.parser')
 listUrl = soup.findAll('a',href=re.compile(r'^/wiki'))
+n=1
 for url in listUrl:
     if not re.search('\.(jpg|JPG)$',url['href']):
-        print url.get_text(),"<------------------------>","https://es.wikipedia.org"+url['href']
+        print n,"<---->",url.get_text(),"<------------------------>","https://es.wikipedia.org"+url['href']
+        n=n+1
